77dedfcd100609137d9958dc2c9ea553c2a7d9c2
Jean-Michel Bodart <jmbodart@skynet.be>
2023-11-23 08:48:08 +0100

    Update Counter.tsx
    Add useRef() and useEffect() to focus the button after 1 sec

--- src/rules-of-hooks/Counter.tsx
+++ src/rules-of-hooks/Counter.tsx
@@ -1,13 +1,23 @@
-import React, { ReactElement, useState } from "react"
+import React, { ReactElement, useState, useRef, useEffect } from "react"
 
 export function Counter(): ReactElement {
   const [counter, setCounter] = useState(0)
+
+  const button = useRef<HTMLButtonElement>(null);
+
+  useEffect( () => {
+    // Automatically focus the button 1 sec after loading
+    setTimeout(() => {
+      button.current?.focus();
+    }, 1000);
+  }, [])
 
   return (
     <div>
       <div>Count: {counter}</div>
       <div>
         <button
+          ref={button}
           className="btn btn-primary"
           onClick={() => setCounter(counter + 1)}
         >
